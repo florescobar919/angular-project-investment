c794b8a562d3e071421d4662768dc5ed01c7672d
Flor Escobar <florescobar919@gmail.com>
2024-08-19 14:04:47 +0200

    changes in 86. Using a Service for Cross-Component Communication

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { HeaderComponent } from "./header/header.component";
 import { UserInputComponent } from "./user-input/user-input.component";
-import { type InvestmentInputModel } from './interfaces/investment-input.model';
 import { InvestmentResultsComponent } from "./investment-results/investment-results.component";
-import { InvestmentResultModel } from './interfaces/investment-result.model';
 
 @Component({
   selector: 'app-root',
@@ -13,30 +11,4 @@ import { InvestmentResultModel } from './interfaces/investment-result.model';
 })
 export class AppComponent {
 
-  resultsData = signal<InvestmentResultModel[] | undefined>(undefined);
-
-  onCalculateInvestmentResults(data: InvestmentInputModel) {
-    const annualData = [];
-    const { initialInvestment, duration, expectedReturn, annualInvestment} = data;
-
-    let investmentValue = initialInvestment;
-  
-    for (let i = 0; i < duration; i++) {
-      const year = i + 1;
-      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-      investmentValue += interestEarnedInYear + annualInvestment;
-      const totalInterest =
-        investmentValue - annualInvestment * year - initialInvestment;
-      annualData.push({
-        year: year,
-        interest: interestEarnedInYear,
-        valueEndOfYear: investmentValue,
-        annualInvestment: annualInvestment,
-        totalInterest: totalInterest,
-        totalAmountInvested: initialInvestment + annualInvestment * year,
-      });
-    }
-    
-    this.resultsData.set(annualData);
-  }
 }
---
+++ src/app/services/investment.services.ts
@@ -0,0 +1,37 @@
+import { Injectable } from "@angular/core";
+import { InvestmentInputModel } from "../interfaces/investment-input.model";
+import { InvestmentResultModel } from "../interfaces/investment-result.model";
+
+@Injectable({providedIn: 'root'})
+export class InvestmentService {
+
+  resultsData? : InvestmentResultModel[];
+
+  calculateInvestmentResults(data: InvestmentInputModel) {
+    const annualData = [];
+    const { initialInvestment, duration, expectedReturn, annualInvestment} = data;
+
+    let investmentValue = initialInvestment;
+  
+    for (let i = 0; i < duration; i++) {
+      const year = i + 1;
+      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+      investmentValue += interestEarnedInYear + annualInvestment;
+      const totalInterest =
+        investmentValue - annualInvestment * year - initialInvestment;
+      annualData.push({
+        year: year,
+        interest: interestEarnedInYear,
+        valueEndOfYear: investmentValue,
+        annualInvestment: annualInvestment,
+        totalInterest: totalInterest,
+        totalAmountInvested: initialInvestment + annualInvestment * year,
+      });
+    }
+
+    this.resultsData = annualData;
+    
+    //this.resultsData.set(annualData);
+  }
+
+}
